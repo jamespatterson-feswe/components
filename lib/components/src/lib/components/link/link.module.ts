fe62ac1535521f5822ebde5cf448f41939237011
James Patterson <jamespatterson.feswe@yahoo.com>
2024-01-29 02:45:13 -0500

    add: focus styles to header links

--- lib/components/src/lib/components/link/link.module.ts
+++ lib/components/src/lib/components/link/link.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 
 import { LinkComponent } from './link.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [LinkComponent],
+  imports: [CommonModule],
   exports: [LinkComponent],
 })
 export class LinkModule {}
